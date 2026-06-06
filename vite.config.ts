b0506c33ea4db104157bf9b776d1f6af004b8cdf
Eugene <59908155+Eugeneiregi@users.noreply.github.com>
2026-06-06 13:01:57 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    proxy: {
+      '/v1': {
+        target: 'https://api.weather-ai.co',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
   },
 });
